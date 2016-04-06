9e6e3a76545950c0a2456c8399d48bfbedf0aa29
Daniel Garcia-Carrillo <garciacarrillo.daniel@yahoo.com>
2016-04-05 19:19:10 -0700

    adds require syntax

--- spec/utils-spec.js
+++ spec/utils-spec.js
@@ -1,23 +1,24 @@
 'use babel'
 /* eslint-env jasmine */
 
-import {IMPORT_REGEX} from '../lib/utils';
+import {capturedDependency} from '../lib/utils';
 
-function expectCorrectMatches(statement, packageName) {
-    const matches = IMPORT_REGEX.exec(statement);
-
-    expect(matches).not.toBe(null);
-    // only full line and package name should be captured
-    expect(matches.length).toEqual(2);
-    expect(matches[1]).toEqual(packageName);
+function expectCorrectMatches(statement, packageName, config={es6Import: true, requireImport: true}) {
+    expect(capturedDependency(statement, config)).toEqual(packageName);
 }
 
 describe('Utils', function() {
-    describe('IMPORT_REGEX', function() {
+    describe('capturedDependency', function() {
         const defaultPackageName = 'package-a_x';
 
         it('handles default import', function() {
             const statement = `import x_a from "${defaultPackageName}"`;
+
+            expectCorrectMatches(statement, defaultPackageName);
+        });
+
+        it('handles require', function() {
+            const statement = `require('${defaultPackageName}')`;
 
             expectCorrectMatches(statement, defaultPackageName);
         });
