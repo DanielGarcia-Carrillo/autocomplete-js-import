0acd2fc87950978e69a64a9388acb91e9c279c16
Daniel Garcia-Carrillo <danielgc@yahoo-inc.com>
2016-08-19 00:48:21 -0700

    Fixes autocompleting after suggestion selected

--- spec/utils-spec.js
+++ spec/utils-spec.js
@@ -16,13 +16,25 @@ describe('Utils', function() {
         const defaultPackageName = 'package-a_x';
 
         it('handles default import', function() {
-            const statement = `import x_a from "${defaultPackageName}"`;
+            const statement = `import x_a from "${defaultPackageName}`;
 
             expectCorrectMatches(statement, defaultPackageName);
         });
 
+        it('rejects when cursor is after "', function() {
+            let statement = `import x_a from "${defaultPackageName}"`;
+
+            expectCorrectMatches(statement, null);
+
+            statement = `require('${defaultPackageName}'`
+            expectCorrectMatches(statement, null);
+
+            statement += ')';
+            expectCorrectMatches(statement, null);
+        });
+
         it('handles require', function() {
-            const statement = `require('${defaultPackageName}')`;
+            const statement = `require('${defaultPackageName}`;
 
             expectCorrectMatches(statement, defaultPackageName);
         });
@@ -36,10 +48,8 @@ describe('Utils', function() {
                 './stuff/blah'
             ].forEach(packageName => {
                 [
-                    `import x from "${packageName}"`,
-                    // no closing quotation mark
                     `import x from "${packageName}`,
-                    `import x from '${packageName}'`
+                    `import x from '${packageName}`
                 ].forEach(importStatement => {
                     expectCorrectMatches(importStatement, packageName);
                 });
@@ -47,32 +57,32 @@ describe('Utils', function() {
         });
 
         it(`handles 'as' imports`, function() {
-            let statement = `import * as x from '${defaultPackageName}'`;
+            let statement = `import * as x from '${defaultPackageName}`;
 
             expectCorrectMatches(statement, defaultPackageName);
 
-            statement = `import {x_a as someFunc} from '${defaultPackageName}'`;
+            statement = `import {x_a as someFunc} from '${defaultPackageName}`;
             expectCorrectMatches(statement, defaultPackageName);
         });
 
         it('handles destructuring import', function() {
-            const statement = `import {x_a} from "${defaultPackageName}"`;
+            const statement = `import {x_a} from "${defaultPackageName}`;
 
             expectCorrectMatches(statement, defaultPackageName);
         });
 
         it('handles inlined multi imports', function() {
-            let statement = `import def, {x_a} from "${defaultPackageName}"`;
+            let statement = `import def, {x_a} from "${defaultPackageName}`;
 
             expectCorrectMatches(statement, defaultPackageName);
 
-            statement = `import {x_a, x_b} from "${defaultPackageName}"`;
+            statement = `import {x_a, x_b} from "${defaultPackageName}`;
             expectCorrectMatches(statement, defaultPackageName);
         });
 
         it('handles multiline import statement', function() {
             // Regex is only meant for prefix so this only tests against last line of import statement
-            const importStatement = `} from "${defaultPackageName}"`;
+            const importStatement = `} from "${defaultPackageName}`;
 
             expectCorrectMatches(importStatement, defaultPackageName);
         });
